0b37588b553dee12386112fc65657b61d550aa7a
Chirag SD <s.chirag.danavendra@gmail.com>
2025-06-29 06:52:51 +0530

    changed user model

--- api/models/User.py
+++ api/models/User.py
@@ -7,7 +7,7 @@ class User(db.Model):
     __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    username = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(255), nullable=False)
     bookmarks = db.relationship("Bookmark", back_populates="user", lazy="dynamic")
 
